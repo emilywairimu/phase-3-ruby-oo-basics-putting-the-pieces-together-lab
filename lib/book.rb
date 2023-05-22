a7d628fc06dd970172bd7b8b65b4d6aaeebedb91
Emily Wairimu <emily.wairimu@student.moringaschool.com>
2023-05-22 02:58:44 -0500

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
-
-
+class Book
+    attr_accessor :title, :page_count, :genre, :author
+  
+    def initialize(title)
+      @title = title
+      @page_count = 0
+    end
+  
+    def turn_page
+      @page_count += 1
+      puts "Flipping the page...wow, you read fast!"
+    end
+  end
+  
+  book = Book.new("And Then There Were None")
+  book.turn_page
+  
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+  
+    def initialize(brand)
+      @brand = brand
+      @condition = "used"
+    end
+  
+    def cobble
+      self.brand = "Nike"
+      self.condition = "new"
+      puts "Your shoe is as good as new!"
+    end
+  end
+  
+  shoe = Shoe.new("Adidas")
+  shoe.cobble
+  
+  
+  
+  
+  
+  
+  
